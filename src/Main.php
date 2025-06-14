875cea0888ef8daf5f497fee2bd7887a3756b05f
Floxy <142106680+floxy0@users.noreply.github.com>
2025-06-14 23:30:42 +0200

    API 5 Update

--- src/Main.php
+++ src/Main.php
@@ -14,20 +14,24 @@ class LobbySystem extends PluginBase implements Listener {
     private Config $config;
 
     public function onEnable(): void {
+        @mkdir($this->getDataFolder());
         $this->saveResource("spawn.yml");
         $this->config = new Config($this->getDataFolder() . "spawn.yml", Config::YAML);
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onJoin(PlayerJoinEvent $event): void {
-        $worldName = $this->config->get("spawn-world");
-        $posString = $this->config->get("spawn-position");
+        $worldName = $this->config->get("spawn-world", "");
+        $posString = $this->config->get("spawn-position", "");
         $parts = explode(" ", $posString);
         if(count($parts) !== 3) return;
-        $x = (float)$parts[0];
-        $y = (float)$parts[1];
-        $z = (float)$parts[2];
+        $x = floatval($parts[0]);
+        $y = floatval($parts[1]);
+        $z = floatval($parts[2]);
         $worldManager = Server::getInstance()->getWorldManager();
+        if(!$worldManager->isWorldLoaded($worldName)) {
+            $worldManager->loadWorld($worldName);
+        }
         $world = $worldManager->getWorldByName($worldName);
         if($world === null) return;
         $player = $event->getPlayer();
